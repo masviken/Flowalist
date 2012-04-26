f6487f38fcba18112d8dbfa0a1d43f684a714045
Tomas Måsviken <tomas.masviken@gmail.com>
2012-04-26 14:36:08 +0200

    Updated repo description

---
+++ myscript.js
@@ -0,0 +1,51 @@
+// Do a couple of console.logs
+console.log("Hello from myscript.js");
+console.log("Hello again!"); // This is not needed
+
+/*
+var name  = prompt("What is your name?");
+alert("Hello " + name);
+
+name = "Nick";
+console.log("The users's name is " + name);
+*/
+
+
+/*console.log("Before");
+
+var name = prompt("What is your name");
+
+if ("Tomas") {
+	console.log("If block");
+} else {
+	console.log("Else block");
+}
+
+console.log("After");*/
+
+/*
+var counter = 10;
+
+while (counter) {
+	console.log("Hello world");
+	console.log("Hi");
+	counter = counter - 1;
+}
+
+while (prompt("What is your name?")) {
+	console.log ("Got your name")
+}
+
+console.log("Before")
+
+for (var counter=10; counter; counter = counter -1) {
+	console.log("Hello world", counter)
+}
+
+console.log("After");
+*/
+
+var friends = ["Nick", "Michael", "Amit", "Tomas";
+console.log(friends);
+console.log(friends.length);
+console.log(friends[1]);
