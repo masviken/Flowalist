6d2171612ea42a1754de39d20a4b9429a9227a06
Tomas Måsviken <tomas.masviken@gmail.com>
2012-05-11 15:17:05 +0200

    Added comments and kept trying to write js.

--- logic.js
+++ logic.js
@@ -1,3 +1,6 @@
+
+// Defines the number of days between today and May 25
+
 var payday = new Date();
 payday.setFullYear(2012,4,25);
 var today = new Date();
@@ -6,14 +9,28 @@ var oneDay = 1000 * 60 * 60 * 24;
 var daysLeft = msLeft / oneDay;
 var weeksLeft = daysLeft / 7;
 
-var balance = 4200;
+// Defines the balance and stores it in localStorage.balance
+
+var balance = 2000;
+localStorage.balance = balance;
 var dayBudget = Math.round(balance / daysLeft);
+var weekBudget = dayBudget * 7;
+
+// Adds a new transaction, writes it on the page and updates localStorage.balance
 
 function transaction () {
 	var amount = prompt("How much did you spend?");
-	var sum = (balance - amount);
-	document.write("You have " + Math.round((balance - amount) / daysLeft) + " kr left to spend today.");
+	
+	if ((dayBudget - amount) < 0) {
+		document.write("Woops, looks like you just spent your daily budget.</br>");
+	} else {
+		document.write("</br>You have " + (dayBudget - amount) + " kr left to spend today.</br>");
+	}
+	//document.write("Your original budget was: </br>" + "Day: " + dayBudget + "</br>Week: " + weekBudget + "</br>Month: " + balance);
+	console.log("Done.");
 };
+
+// Stores the budgets in localStorage keys.... but not really.
 
 var budget = {
 	day: Math.round((balance - transaction) / daysLeft) + " kr",
